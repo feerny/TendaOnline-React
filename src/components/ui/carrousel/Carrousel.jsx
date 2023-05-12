345d6a30248d13871bef68235551ee8f22ba9adc
feerny <luisfpe19@gmail.com>
2023-05-12 11:43:50 -0500

    agrege carouseles y lo adapte para reutilizarlo

--- src/components/ui/carrousel/Carrousel.jsx
+++ src/components/ui/carrousel/Carrousel.jsx
@@ -2,23 +2,26 @@ import React from "react";
 function Carrousel(props) {
   
   return (
-    <div  style={{width:"500px"}} id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+    <div  style={{width:"200px", textAlign:"center"}} id={`carouselExampleAutoplaying${props.idDife}`} class={`carousel ${props.dark?"carousel-dark":""} slide`} data-bs-ride="carousel">
     <div class="carousel-indicators">
-      <button type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-      <button type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide-to="1" aria-label="Slide 2"></button>
-      <button type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide-to="2" aria-label="Slide 3"></button>
+      {props.bestProducts?props.bestProducts.map((data,index)=>
+          <button type="button" data-bs-target={`#carouselExampleAutoplaying${props.idDife}`} data-bs-slide-to={index} class={`${index===0?"active":""}`} aria-current={`${index===0?"true":"false"}`} aria-label={`Slide ${index+1}`}></button>
+      ):null}
+      {/* <button type="button" data-bs-target={`#carouselExampleAutoplaying${props.idDife}`}  data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target={`#carouselExampleAutoplaying${props.idDife}`} data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target={`#carouselExampleAutoplaying${props.idDife}`} data-bs-slide-to="2" aria-label="Slide 3"></button> */}
     </div>
     <div class="carousel-inner">
-      {props.bestProducts.map((data,index)=>
+      {props.bestProducts?props.bestProducts.map((data,index)=>
       <div class={`carousel-item ${index===0?"active":""}`}>
-        <img src={data.image} style={{width:"500px"}} class="d-block" alt="..." />
-      </div>)}
+        <img src={data.image} style={{width:"200px"}} class="d-block" alt="..." />
+      </div>):null}
     </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+    <button class="carousel-control-prev" type="button" data-bs-target={`#carouselExampleAutoplaying${props.idDife}`} data-bs-slide="prev">
       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
       <span class="visually-hidden">Previous</span>
     </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+    <button class="carousel-control-next" type="button" data-bs-target={`#carouselExampleAutoplaying${props.idDife}`} data-bs-slide="next">
       <span class="carousel-control-next-icon" aria-hidden="true"></span>
       <span class="visually-hidden">Next</span>
     </button>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,56 +4,157 @@ import Carrousel from "../../components/ui/carrousel/Carrousel";
 import axios from "axios";
 
 function Home(props) {
-  const [linkNavActivo, setlinkNavActivo] = useState("");
-  const [bestProducts, setbestProducts] = useState([]);
-  useEffect(() => {
-    setlinkNavActivo("nav-link-1");
-    axios
+  async function axiosConsumo(params) {
+    var productsFinals = [];
+    await axios
       .get("https://fakestoreapi.com/products")
       .then(function (response) {
         // handle success
         var arrayrate = [];
         response.data.map((data) => {
-          if (data.category==="men's clothing") {
+          if (data.category === params) {
             arrayrate.push(data.rating.rate);
           }
-
         });
-        console.log(arrayrate);
-        
         arrayrate = arrayrate.filter((valor, indice) => {
           return arrayrate.indexOf(valor) === indice;
         });
         arrayrate = arrayrate.sort((a, b) => b - a);
         arrayrate = [arrayrate[0], arrayrate[1], arrayrate[2]];
-        const products=response.data.filter(dataFilter=>dataFilter.rating.rate===arrayrate[0] || dataFilter.rating.rate===arrayrate[1] || dataFilter.rating.rate===arrayrate[2])
-        var productsFinals=[]
-        productsFinals=products.filter(dataFilter=>dataFilter.rating.rate===arrayrate[0])
-        if (productsFinals.length!==3) {
-            const products2=products.filter(dataFilter=>dataFilter.rating.rate===arrayrate[1]);
-            if (products2.length===1) {
-              products2.map(dataProducts2=>productsFinals.push(dataProducts2))
-            }else if(products2.length>1){
-              var arrayProducts2=[]
-              products2.map(dataProducts2=>arrayProducts2.push(dataProducts2.rating.count))
-              arrayProducts2=arrayProducts2.sort((a, b) => b - a);
-              arrayProducts2=products2.filter(dataFilter=>dataFilter.rating.count===arrayProducts2[0])
-              productsFinals.push(...arrayProducts2);
-            }
+        const products = response.data.filter(
+          (dataFilter) =>
+            dataFilter.rating.rate === arrayrate[0] ||
+            dataFilter.rating.rate === arrayrate[1] ||
+            dataFilter.rating.rate === arrayrate[2]
+        );
+
+        productsFinals = products.filter(
+          (dataFilter) => dataFilter.rating.rate === arrayrate[0]
+        );
+        if (productsFinals.length !== 3) {
+          const products2 = products.filter(
+            (dataFilter) => dataFilter.rating.rate === arrayrate[1]
+          );
+          if (products2.length === 1) {
+            products2.map((dataProducts2) =>
+              productsFinals.push(dataProducts2)
+            );
+          } else if (products2.length > 1) {
+            var arrayProducts2 = [];
+            products2.map((dataProducts2) =>
+              arrayProducts2.push(dataProducts2.rating.count)
+            );
+            arrayProducts2 = arrayProducts2.sort((a, b) => b - a);
+            arrayProducts2 = products2.filter(
+              (dataFilter) => dataFilter.rating.count === arrayProducts2[0]
+            );
+            productsFinals.push(...arrayProducts2);
+          }
         }
-        setbestProducts(productsFinals);
-    })
+      })
       .catch(function (error) {
         console.log(error);
-      })
+      });
+    return productsFinals;
+  }
+  const [linkNavActivo, setlinkNavActivo] = useState("");
+  const [bestProductsRopaHombre, setbestProductsRopaHombre] = useState([]);
+  const [bestProductsJewelwry, setbestProductsJewelwry] = useState([]);
+  const [bestProductselectronics, setbestProductselectronics] = useState([]);
+  const [bestProductsRopaMujer, setbestProductsRopaMujer] = useState([]);
+  useEffect(() => {
+    setlinkNavActivo("nav-link-1");
+    async function fetchData() {
+      const dataRopaHombre = await axiosConsumo("men's clothing");
+      setbestProductsRopaHombre(dataRopaHombre);
+      const datajewelery = await axiosConsumo("jewelery");
+      setbestProductsJewelwry(datajewelery);
+      const dataelectronics = await axiosConsumo("electronics");
+      setbestProductselectronics(dataelectronics);
+      const dataRopaMujer = await axiosConsumo("women's clothing");
+      setbestProductsRopaMujer(dataRopaMujer);
+    }
+    fetchData();
   }, []);
 
   return (
     <div>
       <Nav linkNavActivo={linkNavActivo} />
       <h2>Nuestros mejores productos</h2>
-      <div class="d-flex justify-content-center">
-      <Carrousel bestProducts={bestProducts}/>
+      <div class="row row-cols-1 row-cols-md-2 g-0">
+        <div class="col">
+          <div class="card">
+            <div class="card-img-top">
+              <Carrousel
+                bestProducts={bestProductsRopaHombre}
+                dark={true}
+                idDife={"fdewewfd"}
+              />
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card">
+            <div>
+              <Carrousel
+                bestProducts={bestProductsJewelwry}
+                idDife={"fgdfhgfhs"}
+              />
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card">
+            <div>
+              <Carrousel
+                bestProducts={bestProductsRopaMujer}
+                idDife={"sdfst435"}
+                dark={true}
+              />
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card">
+            <div>
+              <Carrousel
+                bestProducts={bestProductselectronics}
+                idDife={"gfdgteer"}
+              />
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
